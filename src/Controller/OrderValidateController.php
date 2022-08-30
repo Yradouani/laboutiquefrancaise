42cd917b20a890c7a0de1c9a4e559e4293e4794c
Yasmine Radouani <yasmine.radouani@outlook.fr>
2022-08-30 21:54:50 +0200

    integration Stripe

---
+++ src/Controller/OrderValidateController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrderValidateController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/commande/merci/{stripeSessionId}', name: 'order_validate')]
+    public function index($stripeSessionId): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneByStripeSessionId($stripeSessionId);
+
+        if (!$order || $order->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        if (!$order->getIsPaid()) {
+            //Modifier le statut isPaid de la commande en mettant 1 
+            $order->setIsPaid(1);
+            $this->entityManager->flush();
+            //Envoyer un email à notre client pour lui confirmer sa commande
+
+        }
+
+        return $this->render('order_validate/index.html.twig', [
+            'order' => $order
+        ]);
+    }
+}
